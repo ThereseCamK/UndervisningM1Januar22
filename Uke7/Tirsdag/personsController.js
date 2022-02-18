1ac05f7ec9e08a3d7e955fe588f085b96f834765
Therese C Kjær <therese@getacademy.no>
2022-02-18 11:38:56 +0100

    08.02.22

--- Uke7/Tirsdag/personsController.js
+++ Uke7/Tirsdag/personsController.js
@@ -1,8 +1,17 @@
-function addPerson(){
+// const addPerson = () => {
+
+//     model.persons.push({
+//         name: model.inputs.name,
+//         age: model.inputs.age,
+//     });
+//     view();
+// };
+
+function addPerson ()  {
 
     model.persons.push({
         name: model.inputs.name,
         age: model.inputs.age,
     });
     view();
-}
+};
